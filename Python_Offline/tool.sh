cf988bb2e2845140b68fbeae840f5b879d8089bd
Huanle Zhang <zhanghuanle1342@gmail.com>
2021-01-03 23:36:01 -0800

    typo

--- Python_Offline/tool.sh
+++ Python_Offline/tool.sh
@@ -5,5 +5,5 @@ path_to_git=/var/www/html/dtc-personal-website/dtc-google-scholar-helper
 (
 cd ${path_to_git}/Python_Offline
 source activate Python_Offline && python main.py
-scp -i /home/dtc/.ssh/id_rsa google_scholar_citation.txt dtczhl@www.huanlezhang.com:./public_html/dtc-google-scholar-helper/Python_Offline/
+scp -i /home/dtc/.ssh/id_rsa google_scholar_citation.txt user_name@www.huanlezhang.com:./public_html/dtc-google-scholar-helper/Python_Offline/
 )
